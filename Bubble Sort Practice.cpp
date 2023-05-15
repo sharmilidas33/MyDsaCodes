1f4876fe82e05c9e62f715f14973820c2f6b5293
Sharmili Das <128738858+sharmilidas33@users.noreply.github.com>
2023-05-15 14:37:46 +0530

    Add files via upload

---
+++ Bubble Sort Practice.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void printArray(int A[],int n){
+	for(int i=0;i<n;i++){
+		printf("%d ",A[i]);
+	}
+	printf("\n");
+}
+void bubbleSort(int A[], int n){
+	int temp;
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-1-i;j++){
+			if(A[j]>A[j+1]){
+				temp=A[j];
+				A[j]=A[j+1];
+				A[j+1]=temp;
+			}
+		}
+	}
+}
+int main(){
+	int A[]={66,80,65,43,21,64};
+	int n=sizeof(A)/sizeof(int);
+	printArray(A,n);
+	bubbleSort(A,n);
+	printArray(A,n);
+	return 0;
+}
